ec775550ccaeb2589b082c8dadf810034eb51b22
Georges Labreche <georges.labreche@gmail.com>
2012-05-16 03:32:59 -0400

    Initial Commit

---
+++ ViewAnimationColor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewAnimationColor
+//
+//  Created by Georges Labreche on 5/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
